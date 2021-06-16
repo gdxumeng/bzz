20c32580d1154f1fb56204f7f2469008d18e3df7
gdxumeng <gdxumeng@163.com>
2021-06-16 14:09:36 +0800

    适合我的

--- step1.sh
+++ step1.sh
@@ -10,9 +10,9 @@ sudo apt-get install -y screen
 sudo apt-get install -y net-tools
 wget https://github.com/ethersphere/bee/releases/download/v0.6.2/bee_0.6.2_amd64.deb
 wget -O cashout.sh https://gist.githubusercontent.com/ralph-pichler/3b5ccd7a5c5cd0500e6428752b37e975/raw/aa576d6d28b523ea6f5d4a1ffb3c8cc0bbc2677f/cashout.sh && chmod 777 cashout.sh
-wget https://raw.githubusercontent.com/pumpkin4gb/bzz/main/step2.sh && chmod 777 step2.sh
-wget https://raw.githubusercontent.com/pumpkin4gb/bzz/main/step3.sh && chmod 777 step3.sh
-sudo dpkg -i bee_0.6.2_amd64.deb && sudo chown -R bee:bee /var/lib/bee
+wget https://raw.githubusercontent.com/gdxumeng/bzz/main/step2.sh && chmod 777 step2.sh
+wget https://raw.githubusercontent.com/gdxumeng/bzz/main/step3.sh && chmod 777 step3.sh
+sudo dpkg -i bee_0.6.2_amd64.deb && sudo chown -R bee:bee /data/bees
 echo "0" > $cntFile
 chmod +rw $cntFile
 sed -i 's/10000000000000000/1/g' cashout.sh
@@ -33,8 +33,8 @@ echo "    这是第 $tCnt 次创建节点"
 echo "    若需更改endpoint，请自行修改epFile.txt"
 cat>node${tCnt}.yaml<<EOF
 api-addr: :$((1534+${tCnt}))
-#config: /root/node${tCnt}.yaml
-data-dir: /var/lib/bee/node${tCnt}
+#config: /data/node${tCnt}.yaml
+data-dir: /data/bees/node${tCnt}
 cache-capacity: "2000000"
 block-time: "15"
 bootnode:
@@ -43,12 +43,12 @@ debug-api-addr: :$((1634+${tCnt}))
 #debug-api-addr: 127.0.0.1:$((1634+${tCnt}))
 debug-api-enable: true
 p2p-addr: :$((1734+${tCnt}))
-password-file: /var/lib/bee/password
+password-file: /data/bees/password
 swap-initial-deposit: "10000000000000000"
 verbosity: 5
 swap-endpoint: ${ep}
 full-node: true
-welcome-message: "欢迎来到无产阶级社群，MY NAME IS DADAGUAI WECHAT:dislike_diss"
+welcome-message: "Swarm Bzz Bzz Bzz!!!!!!!!!!!!!!"
 EOF
 cp cashout.sh cashout${tCnt}.sh
 sed -i "s/1635/$((1634+${tCnt}))/g" cashout${tCnt}.sh
